e457191b67bfcc069cf3af2aadb8b72751a438ed
Алексей Захаров <zakharov.nnov@gmail.com>
2022-02-05 17:39:07 +0300

    Добавьте файлы проекта.

---
+++ MVVM.cs
@@ -0,0 +1,79 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Prism.Commands;
+using RevitAPITrainingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPICreateDuct
+{
+    public class MVVM
+    {
+        private ExternalCommandData _commandData;
+
+        public List<DuctType> DuctTypes { get; set; } = new List<DuctType>();
+        public List<Level> Levels { get; set; } = new List<Level>();
+        public DelegateCommand SaveCommand { get; }
+        public double DuctOffset { get; set; }
+        public List<XYZ> Points { get; set; } = new List<XYZ>();
+        public DuctType SelectedDuctType { get; set; }
+        public Level SelectedLevel { get; set; }
+        public MEPSystemType DuctSystemType { get; set; }
+
+        public MVVM(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            DuctTypes = DuctsUtils.GetDuctTypes(commandData);
+            Levels = LevelsUtils.GetLevels(commandData);
+            SaveCommand = new DelegateCommand(OnSaveCommand);
+            DuctOffset = 2500;
+            Points = SelectionUtils.GetPoints(_commandData, "Выберите точки", ObjectSnapTypes.Endpoints);
+            DuctSystemType = DuctsUtils.GetDuctSystemTypes(commandData);
+        }
+
+
+        private void OnSaveCommand()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            if (Points.Count < 2 || SelectedDuctType == null || SelectedLevel == null)
+                return;
+
+            XYZ firstPoint = null;
+            XYZ secondPoint = null;
+            for (int i = 0; i < Points.Count; i++)
+            {
+                if (i == 0)
+                    continue;
+                firstPoint = Points[i - 1];
+                secondPoint = Points[i];
+            }
+
+            using (Transaction ts = new Transaction(doc, "Create duct"))
+            {
+                ts.Start();
+
+                Duct duct = Duct.Create(doc, DuctSystemType.Id, SelectedDuctType.Id, SelectedLevel.Id, firstPoint, secondPoint);
+                Parameter parameter = duct.get_Parameter(BuiltInParameter.RBS_OFFSET_PARAM);
+                parameter.Set(UnitUtils.ConvertToInternalUnits(DuctOffset, UnitTypeId.Millimeters));
+                ts.Commit();
+            }
+
+            RaiseCloseRequest();
+        }
+
+        public event EventHandler CloseRequest;
+        private void RaiseCloseRequest()
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("RevitAPICreateDuct")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RevitAPICreateDuct")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("e5659f54-6d5c-490d-972f-1733a5ed5f85")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
